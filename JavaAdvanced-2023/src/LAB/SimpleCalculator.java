a45afcb3c23029f853f26baadffb71883b56104a
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-05-14 22:37:34 +0300

    added Simple Calculator

--- JavaAdvanced-2023/src/LAB/SimpleCalculator.java
+++ JavaAdvanced-2023/src/LAB/SimpleCalculator.java
@@ -5,8 +5,23 @@ import java.util.*;
 public class SimpleCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        String[] input = scanner.nextLine().split(" ");
+        ArrayDeque<String> tokens = new ArrayDeque<>();
+        Collections.addAll(tokens, input);
+        int result = 0;
+        while (tokens.size() > 1) {
 
+            int firstNum = Integer.parseInt(tokens.pop());
+            String sign = tokens.pop();
+            int secondNum = Integer.parseInt(tokens.pop());
+            if (sign.equals("-")) {
+                result = firstNum - secondNum;
+            } else {
+                result = firstNum + secondNum;
 
+            }
+            tokens.push(String.valueOf(result));
+        }
+        System.out.println(tokens.pop());
     }
 }
