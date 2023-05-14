8e06dd3046ba1466b9746d2c1e822935599e821f
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-05-15 01:16:18 +0300

    adding URLs solution

--- JavaAdvanced-2023/src/LAB/BrowserHistory.java
+++ JavaAdvanced-2023/src/LAB/BrowserHistory.java
@@ -1,4 +1,32 @@
 package LAB;
 
+import java.util.*;
+
 public class BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<String> tokens = new ArrayDeque<>();
+        String input = scanner.nextLine();
+        String currentURL = "";
+
+
+        while (!input.equals("Home")) {
+            if (input.equals("back")) {
+
+                if (tokens.size() > 1) {
+                    tokens.pop();
+                    currentURL = tokens.peek();
+                    System.out.println(currentURL);
+                } else {
+                    System.out.println("no previous URLs");
+                }
+            } else {
+                tokens.push(input);
+                currentURL = tokens.peek();
+                System.out.println(currentURL);
+            }
+            input = scanner.nextLine();
+        }
+
+    }
 }
