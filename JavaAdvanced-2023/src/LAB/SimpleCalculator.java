2324ea32e4d6b850fb98dc485a9bfa3badb19da0
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-05-14 22:18:23 +0300

    .

--- JavaAdvanced-2023/src/LAB/SimpleCalculator.java
+++ JavaAdvanced-2023/src/LAB/SimpleCalculator.java
@@ -1,4 +1,12 @@
 package LAB;
 
+import java.util.*;
+
 public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+
+    }
 }
