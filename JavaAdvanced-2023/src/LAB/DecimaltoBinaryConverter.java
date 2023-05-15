4c33d0d8d0e430248d2522bd4fa48463aea31062
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-05-16 00:07:37 +0300

    added decimal to binary

--- JavaAdvanced-2023/src/LAB/DecimaltoBinaryConverter.java
+++ JavaAdvanced-2023/src/LAB/DecimaltoBinaryConverter.java
@@ -2,6 +2,20 @@ package LAB;
 import java.util.*;
 public class DecimaltoBinaryConverter {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int decimal=Integer.parseInt(scanner.nextLine());
+        if(decimal==0){
+            System.out.print(0);
+            return;
+        }
+        ArrayDeque<String> binary=new ArrayDeque<>();
+        while(decimal!=0){
+            binary.push(String.valueOf(decimal%2));
+            decimal/=2;
+        }
+        for (String s : binary) {
+            System.out.printf(s);
+        }
 
     }
 }
