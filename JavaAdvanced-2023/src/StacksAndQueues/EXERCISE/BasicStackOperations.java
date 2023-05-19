af6c851b9570a058c0a77060672849af6696d171
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-05-19 15:51:38 +0300

    updated the last solution

--- JavaAdvanced-2023/src/StacksAndQueues/EXERCISE/BasicStackOperations.java
+++ JavaAdvanced-2023/src/StacksAndQueues/EXERCISE/BasicStackOperations.java
@@ -1,8 +1,44 @@
 package StacksAndQueues.EXERCISE;
+
 import java.util.*;
+import java.util.stream.Collectors;
+
 public class BasicStackOperations {
+    public static void handleElementsPopping(ArrayDeque<Integer> stack, int s) {
+        for (int i = 0; i < s; i++) {
+            stack.pop();
+        }
+    }
+    public static boolean handlePresentChecking(ArrayDeque<Integer> stack, int x){
+
+            if (stack.contains(x)){
+                return true;
+            }
+
+        return false;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        int n = scanner.nextInt(); //number of adds
+        int s = scanner.nextInt();//how many elements to pop
+        int x = scanner.nextInt();// check if its present in the stack
+
+        for (int i = 0; i < n; i++) {
+            stack.push(scanner.nextInt());
+        }
+
+        handleElementsPopping(stack, s);
+        if(handlePresentChecking(stack,x)){
+            System.out.println("true");
+        }else{
+           if(stack.isEmpty()){
+               System.out.println(0);
+           }else{
+               System.out.println(Collections.min(stack));
+               }
+
+           }
+        }
     }
-}
