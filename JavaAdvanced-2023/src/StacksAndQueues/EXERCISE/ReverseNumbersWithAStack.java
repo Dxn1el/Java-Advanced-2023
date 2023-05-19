18ddd1b41e251118ecb70b18e3c0bf46762d4f6f
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-05-19 14:56:35 +0300

    added reversenumbers with stack solution

--- JavaAdvanced-2023/src/StacksAndQueues/EXERCISE/ReverseNumbersWithAStack.java
+++ JavaAdvanced-2023/src/StacksAndQueues/EXERCISE/ReverseNumbersWithAStack.java
@@ -6,9 +6,13 @@ public class ReverseNumbersWithAStack {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayDeque<String> stack = new ArrayDeque<>();
-        String[] inputArr=scanner.nextLine().split("\\s+");
-        while (!stack.isEmpty()){
-            System.out.println(stack.pop());
+        String[] inputArr = scanner.nextLine().split("\\s+");
+
+        for (String s : inputArr) {
+            stack.push(s);
+        }
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
         }
     }
 }
