b351433fbfa5503d564512e77b715f2ec704f062
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-05-19 15:17:50 +0300

    added basic stack operation solution

---
+++ JavaAdvanced-2023/src/StacksAndQueues/EXERCISE/BasicStackOperations.java
@@ -0,0 +1,8 @@
+package StacksAndQueues.EXERCISE;
+import java.util.*;
+public class BasicStackOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
