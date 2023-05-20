cfc72bf6bf4afd9acc0a447c6a0e21eb2885b252
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-05-21 01:01:05 +0300

    added balanced Parentheses

--- JavaAdvanced-2023/src/StacksAndQueues/EXERCISE/BalancedParentheses.java
+++ JavaAdvanced-2023/src/StacksAndQueues/EXERCISE/BalancedParentheses.java
@@ -1,5 +1,52 @@
 package StacksAndQueues.EXERCISE;
 import java.util.*;
 public class BalancedParentheses {
-    Scanner scanner = new Scanner(System.in);
+    public static boolean lenghtChecker(int checker) {
+        if (checker % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static boolean handleBracketsValidation(char lastOpenBracket, char firstOpenBracket) {
+        if (lastOpenBracket == '(' && firstOpenBracket == ')') {
+            return true;
+        } else if (lastOpenBracket == '{' && firstOpenBracket == '}') {
+            return true;
+        } else if (lastOpenBracket == '[' && firstOpenBracket == ']') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<Character> bracketsStack = new ArrayDeque<>();
+        String input = scanner.nextLine();
+
+        if (!lenghtChecker(input.length())) {
+            System.out.println("NO");
+            return;
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            char currentBracket = input.charAt(i);
+            if (currentBracket == '(' || currentBracket == '{' || currentBracket == '[') {
+                bracketsStack.push(currentBracket);
+            } else {
+                if (bracketsStack.isEmpty()) {
+                    System.out.println("NO");
+                    return;
+                } else {
+                    if (handleBracketsValidation(bracketsStack.pop(), currentBracket)) {
+                        continue;
+                    } else {
+                        System.out.println("NO");
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println("YES");
+    }
 }
