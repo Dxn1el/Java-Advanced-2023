28e02eab98c6965a44849952cf492fa046101cdb
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-05-21 01:39:03 +0300

    Update SimpleTextEditor.java

--- JavaAdvanced-2023/src/StacksAndQueues/EXERCISE/SimpleTextEditor.java
+++ JavaAdvanced-2023/src/StacksAndQueues/EXERCISE/SimpleTextEditor.java
@@ -1,4 +1,7 @@
 package StacksAndQueues.EXERCISE;
-
+import java.util.*;
 public class SimpleTextEditor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
