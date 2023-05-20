066e1e98a7119605ba9f42f00f4bd532a6fcda1d
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-05-20 23:12:23 +0300

    Update MaximumElement

--- JavaAdvanced-2023/src/StacksAndQueues/EXERCISE/MaximumElement.java
+++ JavaAdvanced-2023/src/StacksAndQueues/EXERCISE/MaximumElement.java
@@ -1,4 +1,8 @@
 package StacksAndQueues.EXERCISE;
-
+import java.util.*;
 public class MaximumElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
 }
