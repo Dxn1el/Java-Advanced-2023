5d24a2d8b99e4446f61677639a385f8efda9de34
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-04 12:17:00 +0300

    finished sum of matrix

--- JavaAdvanced-2023/src/MultidimensionalArrays/LAB/SumMatrixElements.java
+++ JavaAdvanced-2023/src/MultidimensionalArrays/LAB/SumMatrixElements.java
@@ -1,7 +1,34 @@
 package MultidimensionalArrays.LAB;
+
 import java.util.*;
+
 public class SumMatrixElements {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //3, 6
+        //7, 1, 3, 3, 2, 1
+        //1, 3, 9, 8, 5, 6
+        //4, 6, 7, 9, 1, 0
+
+        String[] rowsAndCols = scanner.nextLine().split(", ");
+        int rows = Integer.parseInt(rowsAndCols[0]);
+        int cols = Integer.parseInt(rowsAndCols[1]);
+        int[][] matrix = new int[rows][cols];
+        int sum = 0;
+
+        for (int i = 0; i < rows; i++) {
+            int[] input = Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).toArray();
+            matrix[i] = input;
+        }
+
+        for (int[] ints : matrix) {
+            for (int anInt : ints) {
+                sum+=anInt;
+            }
+        }
+        System.out.println(rows);
+        System.out.println(cols);
+        System.out.println(sum);
+
     }
 }
