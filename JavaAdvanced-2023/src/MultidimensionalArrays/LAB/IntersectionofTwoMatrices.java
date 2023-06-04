46f65714e961b315bb7434e2012a9a64ffc43e20
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-04 11:47:45 +0300

    finished interception

--- JavaAdvanced-2023/src/MultidimensionalArrays/LAB/IntersectionofTwoMatrices.java
+++ JavaAdvanced-2023/src/MultidimensionalArrays/LAB/IntersectionofTwoMatrices.java
@@ -1,5 +1,40 @@
 package MultidimensionalArrays.LAB;
+
 import java.util.*;
+
 public class IntersectionofTwoMatrices {
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int rows = Integer.parseInt(scanner.nextLine());
+        int cols = Integer.parseInt(scanner.nextLine());
+        char[][] firstMatrix = new char[rows][cols];
+        char[][] secondMatrix = new char[rows][cols];
+
+
+        for (int i = 0; i < rows; i++) {
+            String[] input= scanner.nextLine().split("\\s+");
+            for (int j = 0; j <cols; j++) {
+                firstMatrix[i][j]=input[j].charAt(0);
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            String[] input= scanner.nextLine().split("\\s+");
+            for (int j = 0; j <cols; j++) {
+                secondMatrix[i][j]=input[j].charAt(0);
+            }
+        }
+
+        for (int i = 0; i <rows ; i++) {
+            for (int j = 0; j <cols ; j++) {
+                if(firstMatrix[i][j]==secondMatrix[i][j]){
+                    System.out.print(firstMatrix[i][j]+" ");
+                }else {
+                    System.out.print('*'+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
 }
