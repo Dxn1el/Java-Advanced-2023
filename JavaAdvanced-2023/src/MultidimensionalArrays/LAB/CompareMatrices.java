8a03d4fb111e5288b55ab87ce90963e50bb5fc52
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-04 10:58:54 +0300

    added first exercise

--- JavaAdvanced-2023/src/MultidimensionalArrays/LAB/CompareMatrices.java
+++ JavaAdvanced-2023/src/MultidimensionalArrays/LAB/CompareMatrices.java
@@ -1,4 +1,18 @@
 package MultidimensionalArrays.LAB;
 
+import java.util.*;
+
 public class CompareMatrices {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split("\\s+");
+        int rowsOfFirstMatrix = Integer.parseInt(input[0]);
+        int colsOfSecondMatrix = Integer.parseInt(input[1]);
+        int firstMatrix[][] = new int[rowsOfFirstMatrix][colsOfSecondMatrix];
+        for (int i = 0; i < rowsOfFirstMatrix; i++) {
+            int[] rowOfMatrix = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+            firstMatrix[i] = rowOfMatrix;
+            System.out.println();
+        }
+    }
 }
