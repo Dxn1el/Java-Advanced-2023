e466e7d879b789887d6d70ba2794849ded871307
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-04 11:17:57 +0300

    uprade postitionsOf

--- JavaAdvanced-2023/src/MultidimensionalArrays/LAB/PositionsOf.java
+++ JavaAdvanced-2023/src/MultidimensionalArrays/LAB/PositionsOf.java
@@ -1,4 +1,7 @@
 package MultidimensionalArrays.LAB;
-
+import java.util.*;
 public class PositionsOf {
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+    }
 }
