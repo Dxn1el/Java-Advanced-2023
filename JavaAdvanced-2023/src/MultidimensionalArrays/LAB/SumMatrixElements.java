5f9e3ea37b7ed16a5be2d9eec8376738949904c9
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-04 12:07:33 +0300

    create and update sum of elements

---
+++ JavaAdvanced-2023/src/MultidimensionalArrays/LAB/SumMatrixElements.java
@@ -0,0 +1,7 @@
+package MultidimensionalArrays.LAB;
+import java.util.*;
+public class SumMatrixElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
