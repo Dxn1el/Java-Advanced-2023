3478fbab47569faf8d8cd01cc54c2ef04124ff63
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-06 13:44:11 +0300

    .

---
+++ JavaAdvanced-2023/src/MultidimensionalArrays/EXERCISE/MatrixofPalindromes.java
@@ -0,0 +1,7 @@
+package MultidimensionalArrays.EXERCISE;
+import java.util.*;
+public class MatrixofPalindromes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
