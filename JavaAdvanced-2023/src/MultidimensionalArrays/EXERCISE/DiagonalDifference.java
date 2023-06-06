68722fb7dc7ebe857e0100a50baf27243eac24d6
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-06 10:41:08 +0300

    Update DiagonalDifference.java

--- JavaAdvanced-2023/src/MultidimensionalArrays/EXERCISE/DiagonalDifference.java
+++ JavaAdvanced-2023/src/MultidimensionalArrays/EXERCISE/DiagonalDifference.java
@@ -3,5 +3,9 @@ import java.util.*;
 public class DiagonalDifference {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //3
+        //11 2 4
+        //4 5 6
+        //10 8 -12
     }
 }
