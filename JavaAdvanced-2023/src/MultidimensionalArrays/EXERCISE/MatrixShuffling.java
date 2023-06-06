73b33379c1906f62bd38a7ce1100d70dd820db69
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-06 12:11:54 +0300

    update mixedmatrix

--- JavaAdvanced-2023/src/MultidimensionalArrays/EXERCISE/MatrixShuffling.java
+++ JavaAdvanced-2023/src/MultidimensionalArrays/EXERCISE/MatrixShuffling.java
@@ -1,9 +1,20 @@
 package MultidimensionalArrays.EXERCISE;
 import java.util.*;
 public class MatrixShuffling {
-    public static void main(String[] args) {
+    private static void fillMatrix(int[][] matrix, Scanner scanner) {
+        for (int i = 0; i <matrix.length ; i++) {
+            int[]fillRow=Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            matrix[i]=fillRow;
+        }
+    }
+        public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int rows=scanner.nextInt();
         int cols=scanner.nextInt();
+        int[][]matrix=new int[rows][cols];
+
+        fillMatrix(matrix,scanner);
     }
-}
+
+   }
+
