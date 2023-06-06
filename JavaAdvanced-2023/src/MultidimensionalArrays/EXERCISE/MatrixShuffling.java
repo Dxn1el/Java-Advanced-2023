d2fceda176a8b08efb0f27c13c04f64aae0510b9
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-06 12:24:36 +0300

    updates

--- JavaAdvanced-2023/src/MultidimensionalArrays/EXERCISE/MatrixShuffling.java
+++ JavaAdvanced-2023/src/MultidimensionalArrays/EXERCISE/MatrixShuffling.java
@@ -7,13 +7,26 @@ public class MatrixShuffling {
             matrix[i]=scanner.nextLine().split("\\s+");
         }
     }
+
+    private static boolean validator(String [] commands){
+        return false;
+    }
         public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int rows=scanner.nextInt();
         int cols=scanner.nextInt();
         String[][]matrix=new String[rows][cols];
-
         fillMatrix(matrix,scanner);
+
+        String command=scanner.nextLine();
+        while(!command.equals("end")){
+            String[] commands=command.split("\\s+");
+            int row1=Integer.parseInt(commands[0]);
+            int col1=Integer.parseInt(commands[1]);
+            int row2=Integer.parseInt(commands[2]);
+            int col2=Integer.parseInt(commands[3]);
+            command=scanner.nextLine();
+        }
     }
 
    }
