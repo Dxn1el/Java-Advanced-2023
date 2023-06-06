89accfc80e0ffde002274b0274f6b2cd415d1534
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-06 10:40:39 +0300

    Create DiagonalDifference.java

---
+++ JavaAdvanced-2023/src/MultidimensionalArrays/EXERCISE/DiagonalDifference.java
@@ -0,0 +1,7 @@
+package MultidimensionalArrays.EXERCISE;
+import java.util.*;
+public class DiagonalDifference {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
