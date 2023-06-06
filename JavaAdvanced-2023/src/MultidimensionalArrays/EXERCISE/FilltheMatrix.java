38cf6d73c31d525e69766a3d0b4673b020f3fe1a
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-06 09:46:58 +0300

    Create FilltheMatrix.java

---
+++ JavaAdvanced-2023/src/MultidimensionalArrays/EXERCISE/FilltheMatrix.java
@@ -0,0 +1,7 @@
+package MultidimensionalArrays.EXERCISE;
+import java.util.*;
+public class FilltheMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
