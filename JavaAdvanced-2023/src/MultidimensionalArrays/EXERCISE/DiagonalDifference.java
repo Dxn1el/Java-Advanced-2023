bcd6813cbfe4115b02ce44df07566de9920a0895
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-06 10:58:09 +0300

    `finished diagonal difference

--- JavaAdvanced-2023/src/MultidimensionalArrays/EXERCISE/DiagonalDifference.java
+++ JavaAdvanced-2023/src/MultidimensionalArrays/EXERCISE/DiagonalDifference.java
@@ -1,11 +1,31 @@
 package MultidimensionalArrays.EXERCISE;
 import java.util.*;
 public class DiagonalDifference {
+
+    public static int sumOfDiagonals(int[][] matrix) {
+        int sum=0;
+        for (int rows = 0; rows <matrix.length ; rows++) {
+                sum+=matrix[rows][rows];
+        }
+        for (int i = 0; i <matrix.length ; i++) {
+            sum-=matrix[matrix.length-1-i][i];
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //3
-        //11 2 4
-        //4 5 6
-        //10 8 -12
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int[][] matrix = new int[n][n];
+
+        for (int i = 0; i < matrix.length; i++) {
+            int[] fillTheRow = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            matrix[i] = fillTheRow;
+        }
+        
+        int sum= sumOfDiagonals(matrix);
+        System.out.println(Math.abs(sum));
     }
 }
+
