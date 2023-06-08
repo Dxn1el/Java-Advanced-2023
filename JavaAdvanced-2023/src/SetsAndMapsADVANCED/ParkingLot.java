b4b43043d181876f650cd4dd12f99461250285d3
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-08 23:57:31 +0300

    Update ParkingLot.java

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/ParkingLot.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/ParkingLot.java
@@ -1,4 +1,7 @@
 package SetsAndMapsADVANCED;
-
+import java.util.*;
 public class ParkingLot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
