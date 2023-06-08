1d270a2ddb1696158661147355e6308847fd23e8
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-09 00:16:12 +0300

    Update VoinaNumberGame.java

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/LAB/VoinaNumberGame.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/LAB/VoinaNumberGame.java
@@ -1,4 +1,8 @@
 package SetsAndMapsADVANCED.LAB;
-
+import java.util.*;
 public class VoinaNumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
 }
