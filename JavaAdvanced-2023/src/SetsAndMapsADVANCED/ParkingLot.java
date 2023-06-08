e43c297c2798e6fe9b21fb038d4890f80dfd8b42
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-09 00:15:08 +0300

    finished first problem

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/ParkingLot.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/ParkingLot.java
@@ -3,5 +3,23 @@ import java.util.*;
 public class ParkingLot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Set<String> cars= new LinkedHashSet<>();
+        String input=scanner.nextLine();
+        while (!input.equals("END")){
+            String[]commands=input.split(", ");
+            if(commands[0].equals("IN")){
+                cars.add(commands[1]);
+            }else if(commands[0].equals("OUT")){
+                cars.remove(commands[1]);
+            }
+            input=scanner.nextLine();
+        }
+        if(cars.isEmpty()){
+            System.out.println("Parking Lot is Empty");
+        }else{
+            for (String car : cars) {
+                System.out.println(car);
+            }
+        }
     }
 }
