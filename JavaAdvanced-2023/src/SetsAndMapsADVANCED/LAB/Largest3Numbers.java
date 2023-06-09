f5265956cb53c49bc601faff51ab08ee4f252091
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-10 00:43:08 +0300

    finished 9

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/LAB/Largest3Numbers.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/LAB/Largest3Numbers.java
@@ -1,7 +1,15 @@
 package SetsAndMapsADVANCED.LAB;
+
 import java.util.*;
+
 public class Largest3Numbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .sorted((left, right) -> right.compareTo(left))
+                .limit(3)
+                .forEach(System.out::println);
     }
 }
