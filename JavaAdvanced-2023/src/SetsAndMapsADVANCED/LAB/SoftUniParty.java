4e98840c288618a5c1324f84006915471e0bab97
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-10 01:24:28 +0300

    a

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/LAB/SoftUniParty.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/LAB/SoftUniParty.java
@@ -1,4 +1,34 @@
 package SetsAndMapsADVANCED.LAB;
 
+import com.sun.source.tree.Tree;
+
+import java.util.*;
+
 public class SoftUniParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TreeSet<String> guests = new TreeSet<>();
+        TreeSet<String> VIPguest = new TreeSet<>();
+        String input = scanner.nextLine();
+        while (!input.equals("PARTY")) {
+            if (Character.isDigit(input.charAt(0))) {
+                VIPguest.add(input);
+            } else {
+                guests.add(input);
+            }
+            input = scanner.nextLine();
+        }
+        while (!input.equals("END")) {
+            if (Character.isDigit(input.charAt(0))) {
+                VIPguest.remove(input);
+            } else {
+                guests.remove(input);
+            }
+            input = scanner.nextLine();
+        }
+        System.out.println(guests.size() + VIPguest.size());
+        VIPguest.forEach(System.out::println);
+        guests.forEach(System.out::println);
+    }
+
 }
