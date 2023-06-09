a975376499e05b4fd0880db2ee360b1ccbe5790b
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-09 15:26:02 +0300

    Update CountRealNumbers.java

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/LAB/CountRealNumbers.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/LAB/CountRealNumbers.java
@@ -1,4 +1,7 @@
 package SetsAndMapsADVANCED.LAB;
-
+import java.util.*;
 public class CountRealNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
