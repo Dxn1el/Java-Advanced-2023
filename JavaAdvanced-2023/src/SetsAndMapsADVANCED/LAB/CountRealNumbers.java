91044424a4cd16d58be92023bbcdd80ec67993c1
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-10 00:28:07 +0300

    a

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/LAB/CountRealNumbers.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/LAB/CountRealNumbers.java
@@ -3,5 +3,18 @@ import java.util.*;
 public class CountRealNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double[] input= Arrays.stream(scanner.nextLine().split(" ")).mapToDouble(Double::parseDouble).toArray();
+        LinkedHashMap<Double,Integer> numbers=new LinkedHashMap<>();
+        for (double v : input) {
+            if(numbers.containsKey(v)){
+                int counter= numbers.get(v)+1;
+                numbers.put(v,counter);
+            }else{
+                numbers.put(v,1);
+            }
+        }
+        for (Map.Entry<Double, Integer> entry : numbers.entrySet()) {
+            System.out.printf("%.1f -> %d%n",entry.getKey(),entry.getValue());
+        }
     }
 }
