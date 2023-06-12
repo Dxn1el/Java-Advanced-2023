9e5a78de46f52165c3d49ba88ca263fdaee8316e
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-12 12:42:37 +0300

    Update PeriodicTable.java

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/PeriodicTable.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/PeriodicTable.java
@@ -1,4 +1,7 @@
 package SetsAndMapsADVANCED.EXERCISE;
-
+import java.util.*;
 public class PeriodicTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
