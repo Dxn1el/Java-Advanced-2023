b53c93091dd74b42c122c70576fa7688b09d31bf
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-12 14:21:07 +0300

    a

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/FixEmail.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/FixEmail.java
@@ -3,6 +3,19 @@ import java.util.*;
 public class FixEmail {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        Map<String, String> emailsMap=new LinkedHashMap<>();
+        String name=scanner.nextLine();
+        while(!name.equals("stop")){
+            String mail=scanner.nextLine();
+            if(mail.endsWith("uk") || mail.endsWith("us")||mail.endsWith("com") ){
+
+            }else{
+                emailsMap.put(name,mail);
+            }
+            //uk us com- NO
+
+            name=scanner.nextLine();
+        }
+        emailsMap.entrySet().forEach(k-> System.out.println(k.getKey()+" -> "+k.getValue()));
     }
 }
---
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/HandsOfCards.java
@@ -0,0 +1,7 @@
+package SetsAndMapsADVANCED.EXERCISE;
+import java.util.*;
+public class HandsOfCards {
+    public static void main(String[] args) {
+        
+    }
+}
