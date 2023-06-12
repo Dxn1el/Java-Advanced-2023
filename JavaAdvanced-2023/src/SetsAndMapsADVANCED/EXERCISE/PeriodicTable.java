af27bb975024f39ecc976e56aa04803ad679698b
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-12 12:50:38 +0300

    Update PeriodicTable.java

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/PeriodicTable.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/PeriodicTable.java
@@ -1,7 +1,18 @@
 package SetsAndMapsADVANCED.EXERCISE;
+
 import java.util.*;
+
 public class PeriodicTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Set<String> elements = new TreeSet<>();
+        for (int i = 0; i < n; i++) {
+            String[] input = scanner.nextLine().split("\\s+");
+            for (String s : input) {
+                elements.add(s);
+            }
+        }
+        elements.forEach(l-> System.out.print(l+" "));
     }
 }
