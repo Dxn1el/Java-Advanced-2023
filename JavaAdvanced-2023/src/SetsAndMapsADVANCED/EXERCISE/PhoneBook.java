c1cc28caacd80a65a9616faed8857856de2b80b7
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-12 14:03:42 +0300

    a

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/PhoneBook.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/PhoneBook.java
@@ -1,9 +1,29 @@
 package SetsAndMapsADVANCED.EXERCISE;
+
 import java.util.*;
+
 public class PhoneBook {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Map<String, String> phoneBook = new TreeMap<>();
+        String input=scanner.nextLine();
+        while(!input.equals("search")){
+//John-0888080808
+            String[] number=input.split("-");
+            phoneBook.put(number[0],number[1]);
+            input=scanner.nextLine();
+        }
+        input=scanner.nextLine();
+        while(!input.equals("stop")){
+            String person=input;
+            if(phoneBook.containsKey(person)){
+                System.out.println(person+" -> "+phoneBook.get(person));
+            }else{
+                System.out.println("Contact "+person+" does not exist.");
+            }
 
+            input=scanner.nextLine();
+        }
 
     }
 }
