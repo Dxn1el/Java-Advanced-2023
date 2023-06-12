4b734dabedcc5791859b5a3491ff987e57491bf6
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-12 15:43:56 +0300

    Update UserLogs.java

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/UserLogs.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/UserLogs.java
@@ -1,4 +1,7 @@
 package SetsAndMapsADVANCED.EXERCISE;
-
+import java.util.*;
 public class UserLogs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
