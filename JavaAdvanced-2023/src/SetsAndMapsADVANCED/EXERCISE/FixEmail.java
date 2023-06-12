9b7e4e9d0ed2b435c8a2dab9ce9a345dd11d1bb6
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-12 14:04:06 +0300

    Update FixEmail.java

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/FixEmail.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/FixEmail.java
@@ -1,4 +1,8 @@
 package SetsAndMapsADVANCED.EXERCISE;
-
+import java.util.*;
 public class FixEmail {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
 }
