6951a9322478da511f4a4cf4fc3cffd194c173f3
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-12 13:15:19 +0300

    a

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/PhoneBook.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/PhoneBook.java
@@ -1,4 +1,9 @@
 package SetsAndMapsADVANCED.EXERCISE;
-
+import java.util.*;
 public class PhoneBook {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
 }
