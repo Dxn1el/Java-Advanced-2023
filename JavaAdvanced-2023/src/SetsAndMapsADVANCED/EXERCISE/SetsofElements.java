d1b87de84371eb93028ae3e2e6aca8b49f391075
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-12 12:36:59 +0300

    updates

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/SetsofElements.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/SetsofElements.java
@@ -1,4 +1,24 @@
 package SetsAndMapsADVANCED.EXERCISE;
-
+import java.util.*;
 public class SetsofElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String [] input=scanner.nextLine().split(" ");
+        int n= Integer.parseInt(input[0]);
+        int m= Integer.parseInt(input[1]);
+        Set<Integer> set1=new LinkedHashSet<>();
+        Set<Integer> set2=new LinkedHashSet<>();
+
+        for (int i = 0; i <n ; i++) {
+            set1.add(Integer.parseInt(scanner.nextLine()));
+        }
+
+        for (int i = 0; i <m ; i++) {
+           set2.add(Integer.parseInt(scanner.nextLine()));
+        }
+
+        set1.retainAll(set2);
+        set1.forEach(el->System.out.print(el+" "));
+
+    }
 }
