abb04909316d80cc89751acdec4aeecc3fd66619
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-12 14:28:14 +0300

    update hands of cards

--- JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/HandsOfCards.java
+++ JavaAdvanced-2023/src/SetsAndMapsADVANCED/EXERCISE/HandsOfCards.java
@@ -1,7 +1,19 @@
 package SetsAndMapsADVANCED.EXERCISE;
+
 import java.util.*;
+
 public class HandsOfCards {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        Map<String, Set<String>> handsMap = new LinkedHashMap<>();
+        while (!input.equals("JOKER")) {
+            //"{personName}: {PT, PT, PT,… PT}"
+            String personName = input.split(": ")[0];
+            String personHnd = input.split(": ")[1];
+
+
+            input = scanner.nextLine();
+        }
     }
 }
