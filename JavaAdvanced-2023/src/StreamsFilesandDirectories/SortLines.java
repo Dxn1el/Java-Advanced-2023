ba6332ab5b05766351cca030e4eaa90990e83de9
Dxn1el <126982891+Dxn1el@users.noreply.github.com>
2023-06-19 14:11:53 +0300

    a

--- JavaAdvanced-2023/src/StreamsFilesandDirectories/SortLines.java
+++ JavaAdvanced-2023/src/StreamsFilesandDirectories/SortLines.java
@@ -1,8 +1,22 @@
 package StreamsFilesandDirectories;
+
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+
 public class SortLines {
-    public static void main(String[] args) {
-        Path path= new Path("C:\\Users\\User\\Documents\\GitHub\\JavaAdvanced-2023\\JavaAdvanced-2023\\src\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt");
+    public static void main(String[] args) throws IOException {
+        String pathIN = "C:\\Users\\User\\Documents\\GitHub\\JavaAdvanced-2023\\JavaAdvanced-2023\\src\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+        String pathOUT = "C:\\Users\\User\\Documents\\GitHub\\JavaAdvanced-2023\\JavaAdvanced-2023\\src\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\06.SortLinesOutput.txt";
+
+        Path input = Paths.get(pathIN);
+        Path output = Paths.get(pathOUT);
+        List<String> lines= Files.readAllLines(input);
+        Collections.sort(lines);
+        Files.write(output,lines);
+
+
     }
 }
